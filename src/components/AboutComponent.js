53450ea2f426855d986fae0a2de56b3966af6fa0
Richard Werner <rickyw85@gmail.com>
2021-07-08 21:59:58 -0400

    second commit

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-//testing
-
 export function RenderPartner({partner}) {
     if(partner) {
         return (
